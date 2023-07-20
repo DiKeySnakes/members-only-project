09cf43ea0b35dbc494865972d60fe26691e089c2
DiKeySnakes <dikeysnakes@gmail.com>
2023-07-20 10:40:13 +0300

    Modify .gitignore

--- dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { membership_get, membership_post, } from '../controllers/authController.js';
-import { isAuth } from '../middleware/authMiddleware.js';
-const router = express.Router();
-// GET request to update Membership status.
-router.get('/membership', isAuth, membership_get);
-// POST request to update Membership status.
-router.post('/membership', isAuth, membership_post);
-export default router;
